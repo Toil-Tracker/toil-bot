d74b783e981d21cdd329933249fc4495bd2b84f5
John Paul Herold <johnpaul7@gmail.com>
2020-02-06 13:15:33 -0600

    Print event and context obj
    Before creating any vars

--- serverless/todos/create.py
+++ serverless/todos/create.py
@@ -10,10 +10,10 @@ dynamodb = boto3.resource('dynamodb')
 
 
 def create(event, context):
-    data = json.loads(event['body'])
-    event = json.loads(context)
-    print("Data obj: {}".format(data))
+    #data = json.loads(event['body'])
+    #event = json.loads(context)
     print("Event obj: {}".format(event))
+    print("Context obj: {}".format(context))
     if 'text' not in data:
         logging.error("Validation Failed")
         raise Exception("Couldn't create the todo item.")
